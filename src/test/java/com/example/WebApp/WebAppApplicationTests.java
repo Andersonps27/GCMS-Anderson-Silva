d1764aa62df861f40023b412babe2515c6a6f86c
Andersonps27 <aps27@discente.ifpe.edu.br>
2025-08-28 20:57:39 -0300

    teste quebra proposital Jenkins

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -84,12 +84,13 @@ class WebAppApplicationTests {
             .andExpect(content().string("Projeto GCMS - Prática 03"));
     }
 
+    //teste de quebra proposital
     @Test
     public void testHotfixEndpointMain() throws Exception {
         this.mockMvc.perform(get("/hotfix"))
             .andDo(print())
             .andExpect(status().isOk())
-            .andExpect(content().string("Hotfix (merge main & hotfix)!"));
+            .andExpect(content().string("Hotfix (merge main)!"));
     }
 
 }
